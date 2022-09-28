e0f894d65224589f58bd1c43cab2168b774d0b60
Nielcdn <101749698+Nielcdn@users.noreply.github.com>
2022-09-28 18:04:29 -0300

    Add files via upload

--- js/jogo.js
+++ js/jogo.js
@@ -6,38 +6,73 @@ function tickFunc() {
 		enemy.forEach(p=>p.style.left = parseInt(getComputedStyle(p).left)-2);
 	}
 	if (orient=='r'){
-		enemy.forEach(p=>p.style.left++);
+		pers.forEach(p=>p.style.left = parseInt(getComputedStyle(p).left)+2);
+		enemy.forEach(p=>p.style.left = parseInt(getComputedStyle(p).left)+2);
 	}
 
 	pers.forEach(p=>enemyDestroy(p));
 	pers.forEach(p=>colisao(p));
 
 
-	if (tick==400){
-		let p = document.createElement('div');
-		p.setAttribute('class','pilar');
-		enemy.push(p);
-		pers.push(p);
-		fundo.appendChild(p);
-		p.style.left = getComputedStyle(fundo).width;
+	if (tick==1){
+		createPlatform('chao',450,400,50);
+	}
+	if (tick==700){
+		createPlatform('chao',450,400,50);
+	}
+	if (tick==3000){
+		createPlatform('chao',450,400,50);
+	}
+	if (tick%100==0){
+		createEnemy('pilar',20,0,50);
 	}
 
 	if (tick==500){
-		let p = document.createElement('div');
-		p.setAttribute('class','pilar');
-		enemy.push(p);
-		pers.push(p);
-		fundo.appendChild(p);
-		p.style.left = getComputedStyle(fundo).width;
-		p.style.top = parseInt(getComputedStyle(fundo).height)-20;
+		createEnemy('pilar',20,0,0);
 	}
-
-
+	if (tick==600){
+		createEnemy('pilar',20,0,30);
+	}
+	if (tick==700){
+		createEnemy('pilar',20,0,60);
+	}
 }
 
+function createPlatform(enemyClass,width,leftMin,topMin){
+	let p = document.createElement('div');
+	p.setAttribute('class',enemyClass);
+	pers.push(p);
+	fundo.appendChild(p);
+	p.style.width = width;
+	if (orient=='l'){p.style.left = parseInt(getComputedStyle(fundo).width)-leftMin;}
+	if (orient=='r'){p.style.left = 0;}
+	p.style.top = parseInt(getComputedStyle(fundo).height)-topMin;
+}
+
+
+function createEnemy(enemyClass,width,leftMin,topMin){
+	let p = document.createElement('div');
+	p.setAttribute('class',enemyClass);
+	enemy.push(p);
+	pers.push(p);
+	fundo.appendChild(p);
+	p.style.width = width;
+	if (orient=='l'){p.style.left = parseInt(getComputedStyle(fundo).width)-leftMin;}
+	if (orient=='r'){p.style.left = 0;}
+	p.style.top = parseInt(getComputedStyle(fundo).height)-topMin;
+}
+
+
+
 function gravidade(){
+	if (pers[0] != null){
 	pers.forEach(p=>cair(p));
-	if (cairCheck==0) {pl.style.top = parseInt(getComputedStyle(pl).top)+2;}
+
+	if (cairCheck==0){
+		pl.style.top = parseInt(getComputedStyle(pl).top)+2-grauQueda;		
+	}
+	cairCheck = 0;
+}
 }
 function cair(p){
 	let div1Left= parseInt(getComputedStyle(div1).left);
@@ -52,44 +87,25 @@ function cair(p){
 	
 	let fundoHeight= parseInt(getComputedStyle(fundo).height);
 	let fundoWidth= parseInt(getComputedStyle(fundo).width);
-	if (!(((div1Left >= pLeft)&&(div1Left <= pLeft + pWidth))&&((div1Top == pTop-pHeight - pHeight)&&(div1Top <= pTop ))) && (div1Top <= fundoHeight - div1Height/2)){
-		cairCheck==1
-		pl.style.top = parseInt(getComputedStyle(pl).top)+1;
+	if (cairCheck==0) {
+	if (!((((div1Left >= pLeft)&&(div1Left <= pLeft + pWidth))&&
+	((div1Top >= pTop-div1Height)&&(div1Top <= pTop + pHeight))) || (((pLeft >= div1Left)&&(pLeft <= div1Left + div1Width))&&
+	((pTop >= div1Top)&&(pTop <= div1Top + div1Height)))) && (div1Top <= fundoHeight - div1Height)){
+		cairCheck = 0;
 	}
-	else {cairCheck==0}
+	else {cairCheck = 1;}
+	}
 }
-
 
 function enemyDestroy(p){
-	if (parseInt(getComputedStyle(p).left) <= 0-parseInt(getComputedStyle(p).width)){
-		fundo.removeChild(pers.shift());
+	if (orient=='l' && parseInt(getComputedStyle(p).left) <= 0-parseInt(getComputedStyle(p).width)){
+		fundo.removeChild(pers.splice(pers.indexOf(p),1)[0]);
+	}
+	if (orient=='r' && parseInt(getComputedStyle(p).left) >= parseInt(getComputedStyle(fundo).width)-parseInt(getComputedStyle(p).width )){
+		fundo.removeChild(pers.splice(pers.indexOf(p),1)[0]);
 	}
 }
 
-
-
-function moverdireita(){
-	let div2Left= parseInt(getComputedStyle(div2).left);
-	let fundoWidth= parseInt(getComputedStyle(fundo).width);
-	let div2Width= parseInt(getComputedStyle(div2).width);
-
-	div2.style.left = div2Left +5;
-	if ( div2Left >=  fundoWidth - div2Width){
-		clearInterval(m);
-		m = setInterval ("moveresquerda()",15);
-	}
-}
-function moveresquerda(){
-	let div2Left= parseInt(getComputedStyle(div2).left);
-	let fundoWidth= parseInt(getComputedStyle(fundo).width);
-	let div2Width= parseInt(getComputedStyle(div2).width);
-
-	div2.style.left = div2Left -5;
-	if ( div2Left <= 0){
-		clearInterval(m);
-		m = setInterval ("moverdireita()",15);
-	}
-}
 
 // Mover cursor 1  - através dos botões do teclado
 function move(e) {
@@ -104,16 +120,13 @@ function move(e) {
 		contador ++;
 	}
 
-
-	if (e.keyCode == 65) {
-
-		timer = setInterval("esquerda()",15);
-		contador ++;
-	}
-
 	if (e.keyCode == 32) {
-		timer = setInterval("acima()",15);
+		if (contador==0){countPulo = 0;}
+		if (countPulo==0){
+			timer = setInterval("acima()",50);
+		}
 		contador ++;
+
 	}
 
 	if (e.keyCode == 83) {
@@ -135,71 +148,27 @@ function direita() {
 	}
 }
 
-function esquerda() {
-	let div1Left= parseInt(getComputedStyle(div1).left);
-	
-	div1.style.left = div1Left-5;
-	if ( div1Left <= 0){
-		clearInterval(timer);
-		timer = setInterval ("direita()",15);
-	}
-}
-
-function baixo() {
-	let div1Top= parseInt(getComputedStyle(div1).top);
-	let fundoHeight= parseInt(getComputedStyle(fundo).height);
-	let div1Height= parseInt(getComputedStyle(div1).height);
-
-	div1.style.top = div1Top+5;
-	if(div1Top >= fundoHeight - div1Height){
-		clearInterval(timer);
-	timer = setInterval("acima()",15);
-	}
-}
 function acima() {
-	let div1Top= parseInt(getComputedStyle(div1).top);
-
-	div1.style.top = div1Top-5;
-	if(div1Top <= 0 ){
+	let div1Top = parseInt(getComputedStyle(div1).top)
+	countPulo++;
+	if (countPulo<2){
+		div1Top = parseInt(getComputedStyle(div1).top + 3);
+		grauQueda = grauQueda+3;
+	}
+	if (countPulo<10) {
+		grauQueda = grauQueda+2;
+	}
+	if (countPulo>5) {
+		grauQueda = grauQueda-2;
+	}
+	if (countPulo==20){
 	clearInterval(timer);
-	timer = setInterval("baixo()",15);
+	countPulo = 0;
 	}
 }
 
 // Parar cursores
-function para2(){
-	clearInterval(m);
-}
-function para() {
-	clearInterval(timer);
-}
 
-
-
-function altura(parametro) {
-	div1.style.height = parseInt(getComputedStyle(div1).height)+parametro;
-}
-
-function largura(parametro) {
-	div1.style.width = parseInt(getComputedStyle(div1).width)+parametro;
-}
-function cor(){
-if(cont == 0){
-	div1.style.backgroundColor="blue";
-	cont++;
-}else{
-	if(cont == 1){
-		div1.style.backgroundColor="orange";
-		cont++;
-	   }else{
-	   if(cont == 2){
-		div1.style.backgroundColor="yellow";
-		cont = 0;
-		}
-	}
-}
-
-}
 
 	function colisao(b){
 	let div1Left= parseInt(getComputedStyle(div1).left);
@@ -215,18 +184,6 @@ if(cont == 0){
 	let fundoHeight= parseInt(getComputedStyle(fundo).height);
 	let fundoWidth= parseInt(getComputedStyle(fundo).width);
 
-
-
-		if (((div1Left >= div2Left)&&(div1Left <= div2Left + div2Width))&&
-			((div1Top >= div2Top)&&(div1Top <= div2Top + div2Height))){
-				para2();
-				para();
-		}
-		if (((div2Left >= div1Left)&&(div2Left <= div1Left + div1Width))&&
-			((div2Top >= div1Top)&&(div2Top <= div1Top + div1Height))){
-				para2();
-				para();
-		}
 	}
 
 
@@ -238,28 +195,22 @@ let fundo = document.querySelector('#fundo');
 let pl = document.querySelector('#div1');
 let pers = [];
 let enemy = [];
-let orient = '';
+let orient = 'l';
 let tick = 0;
 let tickInt = setInterval("tickFunc()",10);
 let grav = setInterval("gravidade()",10);
 let audioplay = 0;
 let cairCheck = 0;
-orient = 'l';
-
+let grauQueda = 0;
+let countPulo = 0;
 
 let contador = 0;
-let cont = 0;
-   let m = setInterval("moverdireita()", 15); //Funções que são chamadas a cada 15 e 5 milisegundos
-
+let cont = 0; //Funções que são chamadas a cada 15 e 5 milisegundos
 document.querySelector("body").addEventListener("keydown", (e)=>{move(e)});
 
 
 
 
-let rrrr = document.createElement('div');
-rrrr.setAttribute('class','chao');
-pers.push(rrrr);
-fundo.appendChild(rrrr);
 
 document.querySelector("#para").addEventListener("click",()=>{ para()});
 document.querySelector("#para2").addEventListener("click", ()=>{para2()});
